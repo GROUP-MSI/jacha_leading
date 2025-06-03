65d2e2a356983374109dc6921f2a70b1e60fd056
BENDY369 <demonbendy3691215@gmail.com>
2025-06-03 18:52:03 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ const App = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <h1 className="uppercase">hola a todos</h1>
+      <h3 className="uppercase">esto es de luis</h3>
     </div>
   );
 };

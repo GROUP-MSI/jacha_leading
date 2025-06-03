c844f3654f0e8768608e0ab5df777af696c3a472
ESTEBAN JESUS RAMOS CHAMBILLA <lpze.estebanjesus.ramos.ch@unifranz.edu.bo>
2025-06-03 18:38:14 -0400

    Esteban

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <h1 className="uppercase">hola a todos</h1>
+      <h1 className="uppercase">hola a todafdfsssaos</h1>
     </div>
   );
 };

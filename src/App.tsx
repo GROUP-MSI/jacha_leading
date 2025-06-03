6c70cf521433b29241f3bc144e55b266a549e482
JESUSHMSIS <jeshuanaco7@gmail.com>
2025-06-03 18:40:43 -0400

    add text

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ const App = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <h1 className="uppercase">hola a todos</h1>
+      <h2 className="uppercase">esto es de jesus</h2>
     </div>
   );
 };
